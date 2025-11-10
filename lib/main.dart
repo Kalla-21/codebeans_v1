53f4b75f2f72812c5a4b43963ab5e8efd134137c
markienoodle <mark.balading17@gmail.com>
2025-11-10 16:14:35 +0800

    Connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/landing_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/lesson_detail_screen.dart';
 import 'screens/final_quiz_screen.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
